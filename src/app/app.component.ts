1bf65b78b2f88e0535346eb6bd773a51f64ede4c
Willy <wacb14@gmail.com>
2025-08-15 23:47:34 -0500

    Observable for intervals (setInterval)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { timer } from 'rxjs';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +11,12 @@ import { timer } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'rxjs-learning';
   ngOnInit(): void {
-    //-- Similar to setTimeout but with observables
-    const time = 3000;
-    const trigger = timer(time);
+    //-- Similar to setInterval but with observables
+    const time = 1000;
+    const trigger = interval(time);
 
-    trigger.subscribe(() =>
-      console.log(`This was triigered after ${time/1000} seconds`)
+    trigger.subscribe((n) =>
+      console.log(`This is triggered after ${n} seconds`)
     );
   }
 }
