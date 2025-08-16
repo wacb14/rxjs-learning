5db50d0cb148fc2506d3aa2840b07fa8c93fc8dd
Willy <wacb14@gmail.com>
2025-08-15 23:43:09 -0500

    Observable for timer (setTimeOut)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+  ngOnInit(): void {
+    //-- Similar to setTimeout but with observables
+    const time = 3000;
+    const trigger = timer(time);
+
+    trigger.subscribe(() =>
+      console.log(`This was triigered after ${time/1000} seconds`)
+    );
+  }
 }
