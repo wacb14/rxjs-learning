1d8bf89f437824a368d6aad5999b4ee0380b2283
Willy <wacb14@gmail.com>
2025-08-18 23:31:54 -0500

    The use of tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    const clicks = fromEvent(document, 'click');
+    //-- Tap might be used to debug, but mainly it is used to apply side effects to keep clean main functions as map or merge
+    const positions = clicks.pipe(tap((ev) => console.log('Processed: ', ev)));
+
+    positions.subscribe({
+      next: (pos) => console.log('Next: ', pos),
+      error: (err) => console.error('Error: ', err),
+      complete: () => console.log('Completed'),
+    });
+  }
 }
