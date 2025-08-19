58e5b5657d69bf712a0db8108b36e8bfd9875807
Willy <wacb14@gmail.com>
2025-08-19 00:05:38 -0500

    Use of share to share the original observable for all the subscribers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { map, share, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    const time = timer(1000);
+
+    const obs = time.pipe(
+      tap(() => console.log('Tap is on')),
+      map(() => 'End of obs')
+    );
+
+    const obs1 = obs.subscribe((val) => console.log(val));
+    const obs2 = obs.subscribe((val) => console.log(val));
+
+    //-- The original observable is shared (multicast) to subscribers
+    const sharedObs = obs.pipe(share());
+
+    const obs3 = sharedObs.subscribe((val) => console.log(val));
+    const obs4 = sharedObs.subscribe((val) => console.log(val));
+  }
 }
