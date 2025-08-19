b9abebdcf05f4081b5f1c110f8107d7f41eb97f1
Willy <wacb14@gmail.com>
2025-08-18 22:36:59 -0500

    A converter to observables for events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+  public ngOnInit(): void {
+    const element = document.getElementById('element') as HTMLDivElement;
+    //-- Converts an event as an observable to be able to subscribe and unsubscribe
+    const mouseEvent = fromEvent(element, 'mousemove');
+    mouseEvent.subscribe((e: any) => {
+      console.log(`Coords X:${e.clientX} and Y:${e.clientY}`);
+    });
+  }
 }
