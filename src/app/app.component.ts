235a164fa5e61add9116849d204f9b2bd0ba8471
Willy <wacb14@gmail.com>
2025-08-18 22:49:48 -0500

    Classic filter and map functions applied to observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { filter, map, of, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+  ngOnInit(): void {
+    const numbers = of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+    const evenSquared = pipe(
+      filter((n: number) => n % 2 === 0),
+      map((n) => n * n)
+    );
+
+    const squared = evenSquared(numbers);
+
+    squared.subscribe((e) => console.log(e));
+  }
 }
