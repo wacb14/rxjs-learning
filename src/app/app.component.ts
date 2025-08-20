7f8125bac139b46e4517ec549df62846e7a0cfcd
Willy <wacb14@gmail.com>
2025-08-19 22:28:34 -0500

    Observable cycle of creation and subscription demonstration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+  ngOnInit(): void {
+    const observable = new Observable((e) => {
+      e.next(1);
+      e.next('Hola mundo');
+      e.next(3);
+      // e.error('Error Nro 1'); //-- It is possible to handle error manually
+      e.complete();
+    });
+
+    const subscription = observable.subscribe({
+      next: (x) => console.log(`El siguiente valor es ${x}`),
+      error: (x) => console.error(`Error: ${x}`),
+      complete: () => console.log('Operation was completed successfully!'),
+    });
+
+    subscription.unsubscribe();
+  }
 }
