c7425c8245d3d4547d0d02b0321b6e9177cba12a
Willy <wacb14@gmail.com>
2025-08-19 22:43:17 -0500

    bufferTime use to hold and emit values according time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { bufferTime, interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    const timer = interval(500);
+    //-- Retains values for an interval of a (2000 ms) and outputs them every b (1000 ms).
+    const buffer = timer.pipe(bufferTime(2000,1000));
+
+    buffer.subscribe((v) => console.log(v));
+  }
 }
