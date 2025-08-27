94547361b914cd56886a1040085e3cc19548ab24
Willy <wacb14@gmail.com>
2025-08-26 22:59:32 -0500

    Addtional comments for the counter example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,22 +23,24 @@ export class AppComponent implements OnInit {
   counter = 0;
 
   ngOnInit(): void {
+    //-- Get the buttons
     const pauseButton = document.getElementById('pause');
     const resumeButton = document.getElementById('resume');
-
+    //-- Make the observable counter to rest 1 in each iteration
     const intervalObs = interval(1000).pipe(map(() => -1));
+    //-- Observables to change the "v" value to stop and resume the iterations
     const pause = fromEvent(pauseButton as any, 'click').pipe(map(() => false));
     const resume = fromEvent(resumeButton as any, 'click').pipe(
       map(() => true)
     );
-
+    //-- Merge both observables and force the start of the counter with startWith. Then use switchMap to change between the counter and an empty observable. Next use scan to sum the counter with the initial value (similar to reduce from JS). Finally, use takeWhile to take the value meanwhile the condition is true
     const timer = merge(pause, resume).pipe(
       startWith(true),
       switchMap((v) => (v ? intervalObs : EMPTY)),
       scan((sum, i) => (i ? sum + i : sum), 10),
       takeWhile((i) => i >= 0)
     );
-
+    //-- Subscribe and show the counter
     timer.subscribe((value: any) => (this.counter = value));
   }
 }
