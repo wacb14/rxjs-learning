739bb6a63005fb94254901b85fb7741d3f0158b9
Willy <wacb14@gmail.com>
2025-08-26 22:03:28 -0500

    The basic use of switchMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { fromEvent, interval, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    //-- SwitchMap allows to interrupt the function that we are mapping
+    const count = fromEvent(document, 'click').pipe(
+      switchMap(() => interval(1000))
+    );
+    //-- Every time we click on the document the counter restarts
+    count.subscribe((n) => console.log(n));
+  }
 }
