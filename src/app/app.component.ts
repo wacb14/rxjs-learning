5ea438c03a67fa22903ced3be0ef965e3fa1766d
Willy Cruz <wacb14@gmail.com>
2025-09-10 23:16:43 -0500

    Basic example for forkJoin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { forkJoin, interval, of, take, timeout } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    //-- The forkJoin operator returns the last value of each observable, waiting for the last observable that emits the last value (it ignores the previous values of each observable)
+    const fork = forkJoin([
+      of('Hola'),
+      interval(1000).pipe(take(2)),
+      of('Mundo').pipe(timeout(500)),
+    ]);
+
+    fork.subscribe((c) => console.log(c));
+  }
 }
