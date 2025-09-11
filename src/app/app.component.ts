2a6c8df2103c92a454664ac865c9aac2794d57d9
Willy Cruz <wacb14@gmail.com>
2025-09-11 00:19:49 -0500

    Basic use of concatMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { concatMap, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    const values = of(2000, 1000, 3000);
+    //-- Concatenates the values of an observable to a new and unique observable
+    const maper = values.pipe(
+      concatMap((v) => of(`Value ${v}`).pipe(delay(v)))
+    );
+
+    maper.subscribe((v) => console.log(v));
+  }
 }
