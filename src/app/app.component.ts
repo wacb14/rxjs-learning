fa17a8855e0071db376e5b654d428f8e568adde2
Willy Cruz <wacb14@gmail.com>
2025-09-11 21:49:25 -0500

    Basic example of mergeMap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { delay, mergeMap, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    const values = of(2000, 1000, 3000);
+
+    //-- Similar to concatMap, but this does not wait for observers to finish their executions.
+    const merger = values.pipe(
+      mergeMap((v) => of(`Value ${v}`).pipe(delay(v)))
+    );
+
+    merger.subscribe((v) => console.log(v));
+  }
 }
