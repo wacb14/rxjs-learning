032b8d9307adadf0d29d851fb6bd21009a231d45
Willy Cruz <wacb14@gmail.com>
2025-09-11 22:45:05 -0500

    Using mergeMap to chain multiple dependent observables or queries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { forkJoin } from 'rxjs';
+import { mergeMap } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 @Component({
@@ -13,10 +13,17 @@ export class AppComponent implements OnInit {
   title = 'rxjs-learning';
 
   ngOnInit(): void {
-    //-- forkJoin can be used to make multiple HTTP API requests and return the response from each one as an array.
-    forkJoin([
-      ajax.getJSON('https://api.github.com/users/wacb14'),
-      ajax.getJSON('https://api.github.com/users/google'),
-    ]).subscribe((v) => console.log(v));
+    //-- In this way we can chain multiple requests if the next depends on the content of the previous
+    ajax
+      .getJSON('https://api.github.com/users/ctmil')
+      .pipe(
+        //-- We could use concatMap as well if the order was important
+        mergeMap((res: any) => ajax(res.blog)) //-- Depends of blog from ctmil
+        // mergeMap((blog: any) => ajax(blog.name)), //-- It would depend on name from blog, and so on.
+      )
+      .subscribe({
+        next: (final: any) => console.log(final.status),
+        error: (e) => console.error(e),
+      });
   }
 }
