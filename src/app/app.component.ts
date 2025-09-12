6aaafec8b07bca7a60e50e71d25e62b8ee5a84b2
Willy Cruz <wacb14@gmail.com>
2025-09-11 22:15:30 -0500

    Using forkJoin to chain multiple requests or observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { forkJoin } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rxjs-learning';
+
+  ngOnInit(): void {
+    //-- forkJoin can be used to make multiple HTTP API requests and return the response from each one as an array.
+    forkJoin([
+      ajax.getJSON('https://api.github.com/users/wacb14'),
+      ajax.getJSON('https://api.github.com/users/google'),
+    ]).subscribe((v) => console.log(v));
+  }
 }
